bfad99d517bd28320b996d234fd6b335eb3a6ff8
FMK2312 <fmkhandwala39@gmail.com>
2023-08-11 14:19:06 +0530

    fixed untracked files

--- src/worker.js
+++ src/worker.js
@@ -1,14 +1,16 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
+/*
+ This is the entry point of your worker.
+ you can change the entry point by changing the 'main' in your  
+ wrangler.toml file.
  */
 
 export default {
+	/*
+	This export exposes a fetch listener for your code.
+	Any time your worker recieves a fetch request, this piece of code gets executed.
+	It is mandatory to have an export default in your entry point, with one of the 
+	given event listeners
+	*/
 	async fetch(request, env, ctx) {
 		return new Response('Hello World!');
 	},
